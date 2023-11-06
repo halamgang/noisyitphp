1bc22d5157ca67568a4717270fff68deac05740f
halamgang <73386150+halamgang@users.noreply.github.com>
2023-11-06 10:26:38 +0900

    Update ve_index.php

--- ve_index.php
+++ ve_index.php
@@ -1,8 +1,45 @@
-<?php 
-session_start();
-    // php 모든 시스템은 '/system/php/파일값.php 에 넣어놈 이건 ve_index
-    include_once($_SERVER['DOCUMENT_ROOT'] . '/system/php/ve_index_php.php');
+<?php
+    session_start();
+
+    if (isset($_SESSION['username'])) {
+        $user_username = $_SESSION['username'];
+    }
+
+    require_once 'login/aes.php';
+    $key = 'iU6PYM0YdAqwD4M';
+
+    if (isset($_SESSION['encrypted_name'])) {
+       $decrypted_name = decryptData($_SESSION['encrypted_name'], $key);
+        $username_display = htmlspecialchars($decrypted_name) . "님";
+    } else {
+        $username_display = "Guest";
+    }
+
+    if (isset($_COOKIE['username']) && isset($_COOKIE['session_key'])) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $_COOKIE['username'];
+        session_id($_COOKIE['session_key']);
+    }
+
+    // 관리자 시스템 패널도 가져오기 추가  
+    $user_rank = isset($_SESSION['rank']) ? $_SESSION['rank'] : '';
+
+    function get_user_points($username, $user_data) {
+        foreach ($user_data as $user) {
+            if ($user['username'] === $username) {
+                return $user['point'];
+            }
+        }
+        return 0;
+    }
+
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+        $user_data = json_decode(file_get_contents('./login/users.json'), true);
+        $points = get_user_points($username, $user_data);
+    }
 ?>
+
 <?php
 if (!isset($_COOKIE['noisy_web_pass'])) {
     header("Location: index.php");
